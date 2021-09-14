720471ef65be10ce7920b80d3a62616b4245fd17
Elias Henrique <eliashenriquecoelho0@gmail.com>
2021-09-14 12:08:48 -0300

    Primeira atividade da aula, cálculo de média aritmética

---
+++ aula1/aula1.js
@@ -0,0 +1,15 @@
+var nome = "Elias"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7 
+var notaDoTerceiroBimestre = 8 
+var notaDoQuartoBimestre = 6
+
+var notaFinal = ( notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre )/ 4 
+
+var notaFixada = notaFinal.toFixed(2)
+
+console.log("Bem Vindo " + nome)
+console.log(notaFixada)
+
+// Revisão
+//Variáveis, strings, console.log, toFixed, Operações Matemáticas, concatenação,
